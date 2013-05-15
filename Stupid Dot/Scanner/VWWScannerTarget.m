a38e426cdbbc4d3fb378cf63b9fb863a77d55eed
Zakk Hoyt <zakk@threefoldphotos.com>
2013-05-15 16:52:33 -0700

    added images

--- Stupid Dot/Scanner/VWWScannerTarget.m
+++ Stupid Dot/Scanner/VWWScannerTarget.m
@@ -34,6 +34,48 @@
 #pragma mark Private methods
 
 
++ (NSArray*)getRGBAsFromImage:(UIImage*)image atX:(int)xx andY:(int)yy count:(int)count
+{
+    NSMutableArray *result = [NSMutableArray arrayWithCapacity:count];
+    
+    // First get the image into your data buffer
+    CGImageRef imageRef = [image CGImage];
+    NSUInteger width = CGImageGetWidth(imageRef);
+    NSUInteger height = CGImageGetHeight(imageRef);
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    unsigned char *rawData = (unsigned char*) calloc(height * width * 4, sizeof(unsigned char));
+    NSUInteger bytesPerPixel = 4;
+    NSUInteger bytesPerRow = bytesPerPixel * width;
+    NSUInteger bitsPerComponent = 8;
+    CGContextRef context = CGBitmapContextCreate(rawData, width, height,
+                                                 bitsPerComponent, bytesPerRow, colorSpace,
+                                                 kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+    CGColorSpaceRelease(colorSpace);
+    
+    CGContextDrawImage(context, CGRectMake(0, 0, width, height), imageRef);
+    CGContextRelease(context);
+    
+    // Now your rawData contains the image data in the RGBA8888 pixel format.
+    int byteIndex = (bytesPerRow * yy) + xx * bytesPerPixel;
+    for (int ii = 0 ; ii < count ; ++ii)
+    {
+        CGFloat red   = (rawData[byteIndex]     * 1.0) / 255.0;
+        CGFloat green = (rawData[byteIndex + 1] * 1.0) / 255.0;
+        CGFloat blue  = (rawData[byteIndex + 2] * 1.0) / 255.0;
+        CGFloat alpha = (rawData[byteIndex + 3] * 1.0) / 255.0;
+        byteIndex += 4;
+        
+        UIColor *acolor = [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
+        [result addObject:acolor];
+    }
+    
+    free(rawData);
+    
+    return result;
+}
+
+
+
 #pragma mark Public methods
 
 -(void)setImage:(UIImage*)image{
--- Stupid Dot/VWWViewController.m
+++ Stupid Dot/VWWViewController.m
@@ -9,7 +9,8 @@
 #import "VWWViewController.h"
 
 @interface VWWViewController ()
-
+@property (weak, nonatomic) IBOutlet UIButton *loadImageButton;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @end
 
 @implementation VWWViewController
@@ -25,5 +26,19 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark IBActions
+- (IBAction)loadImageTouchUpInside:(id)sender {
+//    [self.imageView setImage:[UIImage imageNamed:@"white.jpg"]];
+    [self.imageView setImage:[UIImage imageNamed:@"maze_01.jpg"]];
+//    [self.imageView setImage:[UIImage imageNamed:@"maze_02.jpg"]];
+//    [self.imageView setImage:[UIImage imageNamed:@"maze_03.jpg"]];
+//    [self.imageView setImage:[UIImage imageNamed:@"maze_04.jpg"]];
+}
+
+
+
+
+
 
 @end
