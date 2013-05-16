2f7d4695caeee82c47656dbf82ab0e12f1ccc976
Zakk Hoyt <vaporwarewolf@gmail.com>
2013-05-15 22:31:29 -0700

    added links

--- Stupid Dot/controller/VWWViewController.h
+++ Stupid Dot/controller/VWWViewController.h
@@ -5,6 +5,8 @@
 //  Created by Zakk Hoyt on 5/15/13.
 //  Copyright (c) 2013 Zakk Hoyt. All rights reserved.
 //
+//  http://i.imgur.com/sPA4fNK.gif
+//  http://www.youtube.com/watch?v=fBxPYhOnKv0
 
 #import <UIKit/UIKit.h>
 
