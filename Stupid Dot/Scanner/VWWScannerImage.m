8cbd59cdab917841cfdcd2ed8ab78b245d3474de
Zakk Hoyt <zakk@threefoldphotos.com>
2013-05-23 12:43:45 -0700

    added index guard while retrieving pixel from image

--- Stupid Dot/Scanner/VWWScannerImage.m
+++ Stupid Dot/Scanner/VWWScannerImage.m
@@ -52,52 +52,58 @@
 
 
 
-- (NSArray*)getRGBAsFromImage:(UIImage*)image atX:(int)xx andY:(int)yy count:(int)count{
-    NSMutableArray *result = [NSMutableArray arrayWithCapacity:count];
-    
-    // First get the image into your data buffer
-    CGImageRef imageRef = [image CGImage];
-    NSUInteger width = CGImageGetWidth(imageRef);
-    NSUInteger height = CGImageGetHeight(imageRef);
-    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    unsigned char *rawData = (unsigned char*) calloc(height * width * 4, sizeof(unsigned char));
-    NSUInteger bytesPerPixel = 4;
-    NSUInteger bytesPerRow = bytesPerPixel * width;
-    NSUInteger bitsPerComponent = 8;
-    CGContextRef context = CGBitmapContextCreate(rawData, width, height,
-                                                 bitsPerComponent, bytesPerRow, colorSpace,
-                                                 kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
-    CGColorSpaceRelease(colorSpace);
-    
-    CGContextDrawImage(context, CGRectMake(0, 0, width, height), imageRef);
-    CGContextRelease(context);
-    
-    // Now your rawData contains the image data in the RGBA8888 pixel format.
-    int byteIndex = (bytesPerRow * yy) + xx * bytesPerPixel;
-    for (int ii = 0 ; ii < count ; ++ii)
-    {
-        CGFloat red   = (rawData[byteIndex]     * 1.0) / 255.0;
-        CGFloat green = (rawData[byteIndex + 1] * 1.0) / 255.0;
-        CGFloat blue  = (rawData[byteIndex + 2] * 1.0) / 255.0;
-        CGFloat alpha = (rawData[byteIndex + 3] * 1.0) / 255.0;
-        byteIndex += 4;
-        
-        UIColor *acolor = [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
-        [result addObject:acolor];
-    }
-    
-    free(rawData);
-    
-    return result;
-}
-
+//- (NSArray*)getRGBAsFromImage:(UIImage*)image atX:(int)xx andY:(int)yy count:(int)count{
+//    NSMutableArray *result = [NSMutableArray arrayWithCapacity:count];
+//    
+//    // First get the image into your data buffer
+//    CGImageRef imageRef = [image CGImage];
+//    NSUInteger width = CGImageGetWidth(imageRef);
+//    NSUInteger height = CGImageGetHeight(imageRef);
+//    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+//    unsigned char *rawData = (unsigned char*) calloc(height * width * 4, sizeof(unsigned char));
+//    NSUInteger bytesPerPixel = 4;
+//    NSUInteger bytesPerRow = bytesPerPixel * width;
+//    NSUInteger bitsPerComponent = 8;
+//    CGContextRef context = CGBitmapContextCreate(rawData, width, height,
+//                                                 bitsPerComponent, bytesPerRow, colorSpace,
+//                                                 kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+//    CGColorSpaceRelease(colorSpace);
+//    
+//    CGContextDrawImage(context, CGRectMake(0, 0, width, height), imageRef);
+//    CGContextRelease(context);
+//    
+//    // Now your rawData contains the image data in the RGBA8888 pixel format.
+//    int byteIndex = (bytesPerRow * yy) + xx * bytesPerPixel;
+//    for (int ii = 0 ; ii < count ; ++ii)
+//    {
+//        CGFloat red   = (rawData[byteIndex]     * 1.0) / 255.0;
+//        CGFloat green = (rawData[byteIndex + 1] * 1.0) / 255.0;
+//        CGFloat blue  = (rawData[byteIndex + 2] * 1.0) / 255.0;
+//        CGFloat alpha = (rawData[byteIndex + 3] * 1.0) / 255.0;
+//        byteIndex += 4;
+//        
+//        UIColor *acolor = [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
+//        [result addObject:acolor];
+//    }
+//    
+//    free(rawData);
+//    
+//    return result;
+//}
+//
 
 
 
 - (UIColor*)colorFromImageAtX:(float)x andY:(float)y{
 
+
     NSInteger xPixel = self.width * x;
     NSInteger yPixel = self.height * y;
+    
+    if(xPixel >= self.width) xPixel = self.width - 1;
+    if(yPixel >= self.height) yPixel = self.height - 1;
+    
+    
     NSLog(@"Looking for X pixel %d/%d", xPixel, self.width);
     NSLog(@"Looking for Y pixel %d/%d", yPixel, self.height);
     
